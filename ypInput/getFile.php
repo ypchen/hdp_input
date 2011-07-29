f8fa105d32b27e754755a3e50079d053be84dbba
Ying-ping Chen <ypchen@nclab.tw>
2011-07-29 16:50:47 +0800

    Modularized the input methods

--- ypInput/getFile.php
+++ ypInput/getFile.php
@@ -1,6 +1,11 @@
 <?php
 	error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
 
+	unset($rss);
+	if(isset($_GET['rss'])) {
+		$rss = intval($_GET['rss']);
+	}
+
 	unset($file);
 	if(isset($_GET['file'])) {
 		$file = $_GET['file'];
